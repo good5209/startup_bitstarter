a6958bdac8a90013ed78fd846e43f361134ad23c
ailab <ailab@ailab-kvm.(none)>
2013-07-05 23:58:39 +0800

    web.js respond from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fs = require('fs');
+	var content = fs.readFileSync('index.html');
+	//var buf = require('buffer');
+	response.send(content.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
